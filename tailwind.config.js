ca964bea3977bc98da6ef6a7ba7e56b70c8e6a2a
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-09-09 01:46:03 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: [
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [   require('flowbite/plugin')],
+  plugins: [require('flowbite/plugin')],
 }
 
