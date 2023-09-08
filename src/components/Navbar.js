33bcc99517c1deda9088d8071fd12fd135f8afa0
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-09-09 01:40:29 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
 
             <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
                 <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto lg:p-1 p-4">
-                    <a href="https://flowbite.com/" class="flex items-center">
+                    <a href="/" class="flex items-center">
                         <img src={logo} class="h-16 mr-3" alt="Flowbite Logo" />
                         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ram Up Kolkata</span>
                     </a>
