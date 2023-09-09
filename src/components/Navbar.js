56d272e0e6ef64a1fa954eb441087621e512cf7a
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-09-09 10:53:43 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
                 <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto lg:p-1 p-4">
                     <a href="/" class="flex items-center">
                         <img src={logo} class="h-16 mr-3" alt="Flowbite Logo" />
-                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ram Up Kolkata</span>
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ramp Up Kolkata</span>
                     </a>
                     <div class="flex md:order-2">
                         {/* <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button> */}
